c0423103b28e3078bd1f82db8acfc5b5f904839c
dachecker1 <dachecker@ya.ru>
2022-02-27 12:05:22 +0300

    реализовал юзкейсы и интерфейс

--- app/src/main/java/com/vk/dachecker/mathforkids/domain/usecase/GenerateQuestionUseCase.kt
+++ app/src/main/java/com/vk/dachecker/mathforkids/domain/usecase/GenerateQuestionUseCase.kt
@@ -1,4 +1,17 @@
 package com.vk.dachecker.mathforkids.domain.usecase
 
-class GenerateQuestionUseCase {
+import com.vk.dachecker.mathforkids.domain.entity.Question
+import com.vk.dachecker.mathforkids.domain.repository.GameRepository
+
+class GenerateQuestionUseCase(
+    private val repository: GameRepository
+) {
+
+    operator fun invoke(maxSumValue: Int) : Question {
+        return repository.generateQuestion(maxSumValue = maxSumValue, COUNT_OF_OPTIONS)
+    }
+
+    private companion object {
+        private const val COUNT_OF_OPTIONS = 6
+    }
 }
--- app/src/main/java/com/vk/dachecker/mathforkids/domain/usecase/GetGameSettingsUseCase.kt
+++ app/src/main/java/com/vk/dachecker/mathforkids/domain/usecase/GetGameSettingsUseCase.kt
@@ -1,4 +1,14 @@
 package com.vk.dachecker.mathforkids.domain.usecase
 
-class GetGameSettingsUseCase {
+import com.vk.dachecker.mathforkids.domain.entity.GameSettings
+import com.vk.dachecker.mathforkids.domain.entity.Level
+import com.vk.dachecker.mathforkids.domain.repository.GameRepository
+
+class GetGameSettingsUseCase(
+    private val repository: GameRepository
+) {
+
+    operator fun invoke(level: Level) : GameSettings {
+        return repository.getGameSettings(level)
+    }
 }
